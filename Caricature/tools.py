3cc2f4b66facd81ce22d341ee83cba4ca0caef05
UI123456ax <ui123456ax@163.com>
2024-03-16 16:30:48 +0800

    new

---
+++ Caricature/tools.py
@@ -0,0 +1,14 @@
+page = 96
+comic = 'U03'
+tag = [
+    'porn_ero'
+]
+file = [
+    'webp','jpg','png'
+]
+i = 0
+
+while i < page:
+    i = i + 1
+    msg = f"https://gcore.jsdelivr.net/gh/ui123456ax/PicGo/Caricature/{tag[0]}/{comic}/comic/{i}.{file[0]}"
+    print(f'![]({msg})')
