222721e4c80792a74d00f902a52269b4d14c6128
UI123456ax <ui123456ax@163.com>
2024-09-16 14:47:28 +0800

    images update

--- tools.py
+++ tools.py
@@ -1,4 +1,12 @@
-import os,re
+import os,re,shutil
+from PIL import Image
+from threading import Thread
+
+def file_backup(source, path_backup = './Backup/'):
+    # 备份路径
+    if not os.path.exists(path_backup):
+        os.makedirs(path_backup)
+    shutil.move(source, path_backup)
 
 # 漫画页面图片链接
 def comic():
@@ -19,27 +27,85 @@ def comic():
         print(f'![]({msg})')
      
      
-# 博客文章图片链接
-def Auto_Blog_images():
-    def Blog_images(i):
+# 自动替换博客文章图片链接 (path读取路径 || backup是否备份 || path_backup备份文件夹)
+def Auto_Blog_images(path, backup = True, path_backup = '备份'):
+    path = path + '/'
+    def Blog_images(i, passages):
         # 文章
-        passages = 15
         msg = f"![{i}](https://gcore.jsdelivr.net/gh/ui123456ax/PicGo/Blog_images/{passages}_/{passages}_{i}.png)"
         return msg
     
-    # 打开文件
-    with open('./File/read.md','r',encoding='utf-8') as fr, open('./File/write.md','w',encoding='utf-8') as fw :
-        # 读取行数据
-        for line in fr:
-            try:
-                images = re.findall('!\[(\d+)\]\(\)',line)[0]
-                print(images)
-                new_line = line.replace(line, Blog_images(images)+'\n')
-            except:
-                new_line = line
-            fw.write(new_line)
-            
-                    
+    for f_name in [fs for fs in os.listdir(path)]:
+        read_file = path + f_name
+
+        if os.path.isfile(read_file):
+            print(f'开始读取：{f_name}')
+
+            # 文件备份 默认开启
+            if backup:
+                # 文件名，文件格式
+                file_name, file_extension = os.path.splitext(f_name)
+                with open(read_file, 'r', encoding='utf-8') as fr, open(path + path_backup + f'/{file_name}-备份{file_extension}', 'w', encoding='utf-8') as fw:
+                    for line in fr:
+                        fw.write(line)
+
+            # 打开并读取文章数据
+            with open(read_file,'r',encoding='utf-8') as fr, open(read_file,'r+',encoding='utf-8') as fw :
+                try:
+                    passages = re.findall('(.*?)\_', f_name)[0]
+                except:
+                    passages = input('输入指定的文章编号：')
+                # 读取行数据
+                for line in fr:
+                    try:
+                        images = re.findall('!\[(\d+)\]\(\)',line)[0]
+                        new_line = line.replace(line, Blog_images(images, passages)+'\n')
+                        print('{}: {}'.format(passages, images))
+                    except:
+                        new_line = line
+                    fw.write(new_line)
+            print('='*16)
+    
+
+# 图片文件排序 读取路径 输出格式 输出文件夹
+def images_path(path, file_suffix, target_folder = "输出"):
+    target_folder = path + '/' + target_folder
+    image_paths = []
+    # 读取文件列表
+    for f_name in [fs for fs in os.listdir(path)]:
+        file = path + '/' + f_name
+        # 过滤非图片
+        if not os.path.isdir(file):
+            image_paths.append(file)
+    print(image_paths)
+
+    # 转换图片 原路径 输出格式 输出名称 输出路径(文件夹)
+    def convert_and_move_image(image_path, output_format, output_name,  target_folder = 'converted_images'):
+        image = Image.open(image_path)
+        
+        # 转换图片格式
+        converted_image = image.convert('RGB')
+        
+        # 创建目标文件夹，如果不存在
+        if not os.path.exists(target_folder):
+            os.makedirs(target_folder)
+        
+        # 保存转换后的图片到目标文件夹
+        converted_image.save(os.path.join(target_folder, f"{output_name}.{output_format}"))
+        print(image_path, '-->', f"{output_name}.{output_format}")
+
+    threads = []
+    for index,image_path in enumerate(image_paths):
+        thread = Thread(target=convert_and_move_image, args=(image_path, file_suffix, index+1, target_folder))
+        threads.append(thread)
+    for thread in threads:
+        thread.start()
+    for thread in threads:
+        thread.join()
+
+    print(f'共计完成 {len(image_paths)} 个任务')
     
 if __name__ == '__main__':
-    Auto_Blog_images()
+    path = './File/读取'
+    suffix = 'jpg'
+    images_path(path=path, file_suffix=suffix)
