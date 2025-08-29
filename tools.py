a46195e56d28ae08df8d4483e41267ee2de4a8ce
UI123456ax <ui123456ax@163.com>
2025-08-29 21:54:16 +0800

    删除多个文件，包括生成图片列表的脚本、图片列表 JSON 文件、HTML 页面和 API，更新工具脚本以改进文件备份和图片链接处理

--- tools.py
+++ tools.py
@@ -1,15 +1,23 @@
 import os,re,shutil
-# from PIL import Image
+from PIL import Image
 from threading import Thread
 
 def file_backup(source, path_backup = './Backup/'):
-    # 备份路径
+    '''
+    文件备份 (原文件 || 备份路径)
+    '''
+    # 检查备份路径是否存在
     if not os.path.exists(path_backup):
+        # 如果不存在则创建
         os.makedirs(path_backup)
-    shutil.move(source, path_backup)
+    # 复制粘贴文件 shutil.copy || 剪切粘贴文件 shutil.move
+    shutil.copy(source, path_backup)
 
-# 漫画页面图片链接
-def comic():
+
+def comic_link():
+    '''
+    漫画页面图片链接
+    '''
     # 页数
     pages = 96
     # 名称
@@ -23,16 +31,21 @@ def comic():
         'webp','jpg','png'
     ]
     for i in range(1,pages+1):
-        msg = f"https://gcore.jsdelivr.net/gh/ui123456ax/PicGo/Caricature/{tag[0]}/{comic}/comic/{i}.{file[0]}"
+        msg = f"https://gcore.jsdelivr.net/gh/ui123456ax/images/Caricature/{tag[0]}/{comic}/comic/{i}.{file[0]}"
         print(f'![]({msg})')
-     
-     
-# 自动替换博客文章图片链接 (path读取路径 || backup是否备份 || path_backup备份文件夹)
-def Auto_Blog_images(path, backup = True, path_backup = '备份'):
+
+
+def Auto_Blog_images_link(path, backup = True, path_backup = '备份'):
+    '''
+    自动替换博客文章图片链接 (path读取路径 || backup是否备份 || path_backup备份文件夹)
+    规范文档图片: ![图片索引]()
+    规范文档名称: 文章编号_文章主题
+    规范图片名称: 文章编号_图片索引(例:01_01)
+    '''
     path = path + '/'
     def Blog_images(i, passages):
         # 文章
-        msg = f"![{i}](https://gcore.jsdelivr.net/gh/ui123456ax/PicGo/Blog_images/{passages}_/{passages}_{i}.png)"
+        msg = f"![{i}](https://gcore.jsdelivr.net/gh/ui123456ax/images/Blog_images/{passages}_/{passages}_{i}.png)"
         return msg
     
     for f_name in [fs for fs in os.listdir(path)]:
@@ -43,7 +56,7 @@ def Auto_Blog_images(path, backup = True, path_backup = '备份'):
 
             # 文件备份 默认开启
             if backup:
-                # 文件名，文件格式
+                # 将文件以 文件名，文件格式 拆分
                 file_name, file_extension = os.path.splitext(f_name)
                 with open(read_file, 'r', encoding='utf-8') as fr, open(path + path_backup + f'/{file_name}-备份{file_extension}', 'w', encoding='utf-8') as fw:
                     for line in fr:
@@ -51,6 +64,7 @@ def Auto_Blog_images(path, backup = True, path_backup = '备份'):
 
             # 打开并读取文章数据
             with open(read_file,'r',encoding='utf-8') as fr, open(read_file,'r+',encoding='utf-8') as fw :
+                # 获取文章编号 若无则输入
                 try:
                     passages = re.findall('(.*?)\_', f_name)[0]
                 except:
@@ -67,8 +81,14 @@ def Auto_Blog_images(path, backup = True, path_backup = '备份'):
             print('='*16)
     
 
-# 图片文件排序 读取路径 输出格式 输出文件夹
-def images_path(path, file_suffix, target_folder = "输出"):
+def Auto_LearnNotes_images_link(path, backup = True, path_backup = '备份'):
+    pass
+
+
+def images_conversion(path, file_suffix = 'jpg', target_folder = "输出"):
+    '''
+    读取图片转为特定格式并排序 (读取路径 || 输出格式 || 输出文件夹)
+    '''
     target_folder = path + '/' + target_folder
     image_paths = []
     # 读取文件列表
@@ -79,7 +99,7 @@ def images_path(path, file_suffix, target_folder = "输出"):
             image_paths.append(file)
     print(image_paths)
 
-    # 转换图片 原路径 输出格式 输出名称 输出路径(文件夹)
+    # 转换图片 (原路径 || 输出格式 || 输出名称 || 输出文件夹)
     def convert_and_move_image(image_path, output_format, output_name,  target_folder = 'converted_images'):
         image = Image.open(image_path)
         
@@ -104,8 +124,9 @@ def images_path(path, file_suffix, target_folder = "输出"):
         thread.join()
 
     print(f'共计完成 {len(image_paths)} 个任务')
-    
+
+
 if __name__ == '__main__':
     path = './File/读取'
     suffix = 'jpg'
-    Auto_Blog_images(path=path, backup=False)
+    images_conversion(path=path, file_suffix=suffix)
